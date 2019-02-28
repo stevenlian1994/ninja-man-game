921d374e331f55439e3a9f12fa4ae7899563e3fc
Steven Lian <30307988+stevenlian1994@users.noreply.github.com>
2019-02-27 21:57:33 -0800

    ''

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
-var express = require('express');
-var app = express();
-app.get('/', function (req, res) {
-  res.send('Hello World!');
-});
-app.listen(process.env.PORT, process.env.IP, function() {
-    console.log("YelpLakes has started!");
-});
+// var express = require('express');
+// var app = express();
+// app.get('/', function (req, res) {
+//   res.send('Hello World!');
+// });
+// app.listen(process.env.PORT, process.env.IP, function() {
+//     console.log("YelpLakes has started!");
+// });
+alert('hi starting');
 
 // var world = [
 //     [2,2,2,2,2,2,2,2],
