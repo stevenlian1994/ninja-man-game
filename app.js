cdfe72f960422a6f2a02d2f7f03d08c55a0a64ca
Steven Lian <30307988+stevenlian1994@users.noreply.github.com>
2019-02-27 21:51:45 -0800

    ''

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ var app = express();
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+app.listen(process.env.PORT, process.env.IP, function() {
+    console.log("YelpLakes has started!");
 });
 
 // var world = [
